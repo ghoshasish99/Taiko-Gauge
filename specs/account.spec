5edecca338e669c9a464cf9240453b5e796874a5
Ashish Ghosh <Ashish.Ghosh2@ing.com>
2021-02-07 23:28:28 +0100

    specs added

--- specs/account.spec
+++ specs/account.spec
@@ -6,15 +6,7 @@
 ## E-Shop Application - Create New Account
 
 * User launched eshop login page
-* User create account with "<FirstName>", "<LastName>", "<EmailID>" and "<Password>"
+* User create account with <FirstName>, <LastName>, <EmailID> and <Password>
 * User account should get created
 
-| EmailID                    | Password  |
-|----------------------------|-----------|
-| testuser_negative@shop.com | Testing$1 |
 
-## Login to the E-Shop Application with Wrong Password
-
-* User launched eshop login page
-* User logged in eshop using the invalid emailid "<EmailID>" and the invalid password "<Password>"
-* User should not get logged in
--- tests/step_implementation.js
+++ tests/step_implementation.js
@@ -1,27 +1,42 @@
-/* globals gauge*/
 "use strict";
-const { openBrowser,write, closeBrowser, goto, press, text, focus, textBox, toRightOf } = require('taiko');
-const assert = require("assert");
-const headless = process.env.headless_chrome.toLowerCase() === 'true';
+const {openBrowser,write, closeBrowser, goto, text, textBox} = require('taiko')
+const assert = require('assert')
 
 beforeSuite(async () => {
-    await openBrowser({ headless: headless })
+    await openBrowser(/*{ headless: false }*/)
 });
 
 afterSuite(async () => {
     await closeBrowser();
 });
 
-step("Goto getgauge github page", async () => {
-    await goto('https://github.com/getgauge');
+step("User launched eshop login page", async () => {
+    //await openBrowser();
+    await goto('http://awswrkshpalb-1570520390.us-west-2.elb.amazonaws.com:3000/cts-shop/login');
 });
 
-step("Search for <query>", async (query) => {
-    await focus(textBox(toRightOf('Pricing')))
-    await write(query);
-    await press('Enter');
+step("User logged in eshop using the invalid emailid <EmailID> and the invalid password <Password>", async (username,password) => {
+    await write(username,into(textBox('Email')),{force:true});
+    await write(password,into(textBox('password')),{force:true});
+    await click('LOG IN');
 });
 
-step("Page contains <content>", async (content) => {
-    assert.ok(await text(content).exists());
+step("User should not get logged in", async () => {
+    assert.ok(await text('Customer not found').exists());
+});
+
+step("User create account with <FirstName>, <LastName>, <EmailID> and <Password>", async (fname, lname, email, password) => {
+    let random = Math.floor(Math.random()*90000) + 10000;
+    email = email.replace('Ashish','Ashish'+random);
+    await click('CREATE YOUR E-SHOP ACCOUNT');
+    await write(fname,into(textBox('first name')));
+    await write(lname,into(textBox('last name')));
+    await write(email,into(textBox('email')));
+    await write(password,into(textBox('password')));
+    await write(password,into(textBox('confirm password')));
+    await click('CREATE YOUR E-SHOP ACCOUNT');
+});
+
+step("User account should get created", async () => {
+    assert.ok(await textBox('search product').exists());
 });
